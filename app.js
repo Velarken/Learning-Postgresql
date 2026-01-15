0b93a0d10ff6808efc7035757abecb8ad6371dd8
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-01-15 09:28:27 -0500

    added middleware to handle parsing request body

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const PORT = 3030;
 
 app.set('view engine', 'ejs');
+app.use(express.json()) // parse req.body
 app.use(express.urlencoded({ extended: true }))
 
 // static resources
