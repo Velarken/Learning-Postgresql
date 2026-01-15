f78d5d542b1544587d188de5e96fa9c644f0b3f9
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-01-15 09:28:59 -0500

    updated search function, added delete function

--- controllers/newUserController.js
+++ controllers/newUserController.js
@@ -21,17 +21,17 @@ async function createUsernamePost(req, res) {
 async function searchUserGet(req,res) {
     console.log('Searching for: ', req.query.username)
     const SQL = `SELECT username FROM usernames WHERE username LIKE '%${req.query.username}%';`
+    const client = new Client({
+    connectionString: `postgresql://${process.env.DB_NAME}:${process.env.DB_PASS}@localhost:5432/top_users`,
+    });
     async function query() {
         try {
             console.log('Starting search process')
-            const client = new Client({
-            connectionString: `postgresql://${process.env.DB_NAME}:${process.env.DB_PASS}@localhost:5432/top_users`,
-            });
             await client.connect();
             const response = await client.query(SQL);
             await client.end()
             res.render('searchResults.ejs', {data:response.rows})
-            console.table(response.rows[0]);
+            console.table(response.rows);
         } catch(err) {
             console.log(err)
         } finally {
@@ -41,9 +41,29 @@ async function searchUserGet(req,res) {
     query();
 }
 
+async function deleteUserPost(req,res) {
+    console.log('Deleting selected user:', req.body.username)
+    const SQL = `DELETE FROM usernames WHERE username = '${req.body.username}'`;
+    const client = new Client({
+    connectionString: `postgresql://${process.env.DB_NAME}:${process.env.DB_PASS}@localhost:5432/top_users`,
+    });
+    try {
+        console.log('Deleting user.');
+        await client.connect();
+        const response = await client.query(SQL);
+        await client.end();
+    } catch(err) {
+        console.log('Something went wrong.', err)
+    } finally {
+        console.log('User deleted', req.body.username)
+        res.redirect('/');
+    }
+}
+
 module.exports = {
   getUsernames,
   createUsernameGet,
   createUsernamePost,
-  searchUserGet
+  searchUserGet,
+  deleteUserPost
 };
